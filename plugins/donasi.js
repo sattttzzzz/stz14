503f891a38f771678a3ef9fad56d13bef27a3df7
sattttzzzz <121809716+sattttzzzz@users.noreply.github.com>
2023-01-02 21:51:19 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
             .✵.𝗚𝗥𝗨𝗣 𝗣𝗘𝗡𝗧𝗔𝗕𝗢𝗧.✵.
 
-            https://chat.whatsapp.com/ElBB5NrJS637TYqjJLHuk2
+            https://chat.whatsapp.com/Ceyf9fqMNWe9nHC7dG9Fag
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
