79f607974fe79ed32237cc2003f5759bc2a40975
sattttzzzz <121809716+sattttzzzz@users.noreply.github.com>
2023-01-02 21:47:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 let fs = require('fs')                    //CREATOR = GEMPY , DEVBOT
 let chalk = require('chalk')
 global.owner = [
-  ['6287898307350'],
-  ['66287898307350'],
-  ['6287898307350', 'gempy', true]  
+  ['6288213793536'],
+  ['6288219923569'],
+  ['6288213793536', 'Sattzzzz', true]  
   
 ] 
 global.ownername = [['Gempy']]
-global.namabot = [['PENTABOT']]
-global.ownerinsta = [['@gmpyyy']]
+global.namabot = [['PANDABOT']]
+global.ownerinsta = [['@onlyskyyyy_']]
 global.mods = [] 
 global.prems = [] 
 
@@ -43,8 +43,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'PENTABOT'
-  var sticker_author = 'By GEMPY'
+  var sticker_name = 'PANDABOT'
+  var sticker_author = 'By Sattzzzz'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -61,7 +61,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'Created by Gempy'
+global.wm = 'Created by Sattzzzz'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
